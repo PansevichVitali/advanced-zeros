43ee7f7dea3fa33b6984b4aab3b688a3c414a2f0
PansevichVitali <egyptiain94@gmail.com>
2019-02-22 00:38:36 +0300

    Improve solution, fix algorithm

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 module.exports = function getZerosCount(number, base) {
   let prime = getPrime();
-  let powerPrime = 1;
+  let powerPrime = 0;
   let NumZero = 0;
-  if(Math.sqrt(base) === Math.floor(Math.sqrt(base)))
-    powerPrime++;
-  for(let i = 1; number > Math.pow(prime, i);i++){
+  for(let i = 1; number >= Math.pow(prime, i); i++)  {
     NumZero += Math.floor(number/Math.pow(prime, i));
+  }
+  while(base%prime === 0) {
+    base /= prime;
+    powerPrime++;
   }
 
   function getPrime() {
-    for(let i = Math.floor(base/2); i > 2; i--) {
-     if (base/i === Math.floor(base/i)) {
-       for(let j = i-1; j > 1; j--) {
+    for(let i = base-1; i >= 2; i--) {
+      if (base/i === Math.floor(base/i)) {
+        if(i == 2 && base/i === Math.floor(base/i)) 
+          return 2;
+        for(let j = i-1; j > 1; j--) {
           if(i/j === Math.floor(i/j))
             break;
-          if(i/j !== Math.floor(i/j) && j === 2)
+          if(i/j !== Math.floor(i/j) && j === 2 && i !== 2)
             return i;
-       }
-     } 
+        }
+      } 
     }
     return base;
   }
