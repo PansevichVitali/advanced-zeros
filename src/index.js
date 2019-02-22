15974c7d82765b5df9460b9fa1a8289dd5937718
PansevichVitali <egyptiain94@gmail.com>
2019-02-22 15:21:05 +0300

    no limit to refact

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  let prime = getPrime();
+  const prime = getPrime(base);
   let powerPrime = 0;
-  let NumZero = 0;
+  let qtZeros = 0;
+  let cpyBase = base;
   for(let i = 1; number >= Math.pow(prime, i); i++)  {
-    NumZero += Math.floor(number/Math.pow(prime, i));
+    qtZeros += Math.floor(number/Math.pow(prime, i));
   }
-  while(base%prime === 0) {
-    base /= prime;
+  while(cpyBase%prime === 0) {
+    cpyBase /= prime;
     powerPrime++;
   }
 
-  function getPrime() {
+  function getPrime(base) {
     for(let i = base-1; i >= 2; i--) {
       if (base/i === Math.floor(base/i)) {
-        if(i == 2 && base/i === Math.floor(base/i)) 
+        if(i === 2 && base/i === Math.floor(base/i)) 
           return 2;
         for(let j = i-1; j > 1; j--) {
           if(i/j === Math.floor(i/j))
             break;
-          if(i/j !== Math.floor(i/j) && j === 2 && i !== 2)
+          if(i/j !== Math.floor(i/j) && j === 2)
             return i;
         }
       } 
     }
     return base;
   }
-  return Math.floor(NumZero/powerPrime);
+  return Math.floor(qtZeros/powerPrime);
 }
