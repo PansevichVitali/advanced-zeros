b7724b41a4526c0780567281adae5e506467d687
PansevichVitali <egyptiain94@gmail.com>
2019-02-27 22:38:29 +0300

    99 test passed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  const prime = getPrime(base);
+  let prime = getPrime(base);
   let powerPrime = 0;
-  let qtZeros = 0;
+  let qttZeros = 0;
   let cpyBase = base;
-  for(let i = 1; number >= Math.pow(prime, i); i++)  {
-    qtZeros += Math.floor(number/Math.pow(prime, i));
-  }
   while(cpyBase%prime === 0) {
     cpyBase /= prime;
     powerPrime++;
   }
-
+  while(Math.pow(prime,2) < cpyBase) {
+    prime = getPrime(cpyBase);
+    powerPrime = 0;
+    while(cpyBase%prime === 0) {
+      cpyBase /= prime;
+      powerPrime++;
+    }
+  }
+  for(let i = 1; number >= Math.pow(prime, i); i++) {
+    qttZeros += Math.floor(number/Math.pow(prime, i));
+  }
+ 
   function getPrime(base) {
     for(let i = base-1; i >= 2; i--) {
       if (base/i === Math.floor(base/i)) {
@@ -26,5 +34,5 @@ module.exports = function getZerosCount(number, base) {
     }
     return base;
   }
-  return Math.floor(qtZeros/powerPrime);
+  return Math.floor(qttZeros/powerPrime);
 }
