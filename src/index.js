3805ff6ad39ec5c80191920b20ad4f609b44dfcb
PansevichVitali <egyptiain94@gmail.com>
2019-03-23 01:49:42 +0300

    style: change white-space

--- src/index.js
+++ src/index.js
@@ -4,37 +4,41 @@ module.exports = function getZerosCount(number, base) {
   let qttZeros = 0;
   let cpyBase = base;
   let cpyBaseFinal = base;
-  while(cpyBase%prime === 0) {
+
+  while (cpyBase%prime === 0) {
     cpyBase /= prime;
     powerPrime++;
   }
-  if(Math.pow(prime,2) < cpyBase) {
+
+  if (Math.pow(prime,2) < cpyBase) {
     cpyBase = base/prime;
     prime = getPrime(cpyBase);
     powerPrime = 0;
-    while(cpyBaseFinal%prime === 0) {
+    while (cpyBaseFinal%prime === 0) {
       cpyBaseFinal /= prime;
       powerPrime++;
     }
   }
-  for(let i = 1; number > Math.pow(prime, i); i++) {
+
+  for (let i = 1; number > Math.pow(prime, i); i++) {
     qttZeros += Math.floor(number/Math.pow(prime, i));
   }
  
   function getPrime(base) {
-    for(let i = base-1; i >= 2; i--) {
+    for (let i = base-1; i >= 2; i--) {
       if (base/i === Math.floor(base/i)) {
-        if(i === 2 && base/i === Math.floor(base/i)) 
+        if (i === 2 && base/i === Math.floor(base/i)) 
           return 2;
-        for(let j = i-1; j > 1; j--) {
-          if(i/j === Math.floor(i/j))
+        for (let j = i-1; j > 1; j--) {
+          if (i/j === Math.floor(i/j))
             break;
-          if(i/j !== Math.floor(i/j) && j === 2)
+          if (i/j !== Math.floor(i/j) && j === 2)
             return i;
         }
       } 
     }
     return base;
   }
+
   return Math.floor(qttZeros/powerPrime);
 }
